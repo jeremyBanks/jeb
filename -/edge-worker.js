1b8c2438f1e47784e441e56eacf6b6dd27dfc93e
stadian <stadian@stadians.dev>
2020-09-17 02:03:44 +0000

    r340 nine eight eight three

--- -/edge-worker.js
+++ -/edge-worker.js
@@ -1,66 +1,71 @@
-// import { slugify } from "./index.js";
+import { slugify } from "./index.js";
 
-// addEventListener("fetch", event =>
-//   event.respondWith(
-//     (async () => {
-//       const request = event.request;
-//       const url = new URL(request.url);
-//       const redirect = await maybeRedirect(url);
+addEventListener(
+  "fetch",
+  event => null,
+); /*
 
-//       if (redirect) {
-//         return redirect;
-//       } else {
-//         return fetch(request);
-//       }
-//     })(),
-//   ),
-// );
+  event.respondWith(
+    (async () => {
+      const request = event.request;
+      const url = new URL(request.url);
+      const redirect = await maybeRedirect(url);
 
-// const maybeRedirect = async url => {
-//   if (url.host !== "stadia.run") {
-//     return null;
-//   }
+      if (redirect) {
+        return redirect;
+      } else {
+        return fetch(request);
+      }
+    })(),
+  ),
+);
 
-//   const pathSegments = url.pathname.slice(1).split(/\//g);
-//   if (pathSegments.length === 1 && pathSegments[0] === "") {
-//     pathSegments.pop();
-//   }
+const maybeRedirect = async url => {
+  if (url.host !== "stadia.run") {
+    return null;
+  }
 
-//   if (pathSegments.length !== 1) {
-//     return null;
-//   }
+  const pathSegments = url.pathname.slice(1).split(/\//g);
+  if (pathSegments.length === 1 && pathSegments[0] === "") {
+    pathSegments.pop();
+  }
 
-//   const slug = pathSegments[0];
-//   if (slugify(slug) !== slug) {
-//     return null;
-//   }
+  if (pathSegments.length !== 1) {
+    return null;
+  }
 
-//   let skus;
-//   try {
-//     skus = await (
-//       await fetch("https://stadia.st/-/skus.json", {
-//         cf: {
-//           cacheTtlByStatus: {
-//             200: 1 * 60 * 60,
-//           },
-//         },
-//       })
-//     ).json();
-//   } catch (error) {
-//     console.error(error);
-//     return null;
-//   }
+  const slug = pathSegments[0];
+  if (slugify(slug) !== slug) {
+    return null;
+  }
 
-//   const game = Object.values(skus).find(
-//     sku => sku.type === "game" && slugify(sku.name) == slug,
-//   );
+  let skus;
+  try {
+    skus = await (
+      await fetch("https://stadia.st/-/skus.json", {
+        cf: {
+          cacheTtlByStatus: {
+            200: 1 * 60 * 60,
+          },
+        },
+      })
+    ).json();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 
-//   if (!game) {
-//     return null;
-//   }
+  const game = Object.values(skus).find(
+    sku => sku.type === "game" && slugify(sku.name) == slug,
+  );
 
-//   return Response.redirect(
-//     `https://stadia.google.com/player/${game.appId}`,
-//     301,
-//   );
-// };
+  if (!game) {
+    return null;
+  }
+
+  return Response.redirect(
+    `https://stadia.google.com/player/${game.appId}`,
+    301,
+  );
+};
+*/
