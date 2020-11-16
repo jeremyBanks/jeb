72ecc3dd634fb380701199d4ae517aa5836c52ee
Jeremy Banks <_@jeremy.ca>
2020-11-15 19:43:49 -0500

    m

---
+++ deno-stadia/aes-gcm-256-wasm/pkg/aes_gcm_256_wasm_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function aes_gcm_256_decrypt_and_verify_as_utf8(a: number, b: number, c: number, d: number, e: number, f: number, g: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
