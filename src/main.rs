07299932dd3e2ad88857ed581a76b272c6b940a7
Jeremy Banks <_@jeremy.ca>
2021-10-27 03:01:20 +0000

    r1 one four delta eight

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-#[allow(unused_imports)]
-use tracing::{
-    debug, debug_span, error, error_span, info, info_span, instrument, span, trace, trace_span,
-    warn, warn_span,
-};
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Initialize global unhandled error panic handler.
     color_eyre::install()?;
+
+    // Initialize global logging handler.
     tracing_subscriber::util::SubscriberInitExt::init(tracing_subscriber::Layer::with_subscriber(
         tracing_error::ErrorLayer::default(),
         tracing_subscriber::fmt()
@@ -13,27 +11,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .finish(),
     ));
 
-    info!("Hello, world!");
-
-    let x = get_a_number();
-
-    info!(x, "Got a number!");
+    log::info!("hello, log!");
+    tracing::info!("hello, tracing!");
 
     Ok(())
-}
-
-#[instrument(level = "info")]
-fn get_a_number() -> i32 {
-    do_some_math(3)
-}
-
-#[instrument(level = "debug")]
-fn do_some_math(n: i32) -> i32 {
-    let base = {
-        let _ = info_span!("doing some difficult math").enter();
-
-        "3".parse::<i32>().unwrap()
-    };
-
-    base * n
 }
