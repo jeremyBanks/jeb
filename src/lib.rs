88af09ecc7e21205f611d3d9cb055c824966bfc8
Jeremy Banks <_@jeremy.ca>
2021-10-31 16:36:16 +0000

    r13 five six foxtrot foxtrot

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Serialize};
 use serde_json::{json, Value as Json};
 
 pub mod client;
@@ -7,13 +8,20 @@ pub mod spider;
 #[tracing::instrument(name = "stadians")]
 pub async fn main() {
     let google_cookie = std::env::var("GOOGLE_COOKIE").expect("GOOGLE_COOKIE not set");
-    let db = rusqlite::Connection::open("./x.sqlite").expect("unable to open sqlite db");
+
+    let cache = kv::Store::new(kv::Config::new("cache.ignore").cache_capacity(536_870_912))
+        .unwrap()
+        .bucket::<&str, kv::Json<ApiCall>>(None)
+        .unwrap();
 
     let mut spider = crate::spider::Spider::new(google_cookie);
     let result = spider
         .client
-        .api_request(&[("D0Amud", json!([Json::Null, true]))])
+        .api_request(&[("FdyJ0", json!(["j e"]))])
         .await
         .unwrap();
     tracing::info!("we got something! {}", result[0].to_string());
 }
+
+#[derive(Serialize, Deserialize)]
+struct ApiCall {}
