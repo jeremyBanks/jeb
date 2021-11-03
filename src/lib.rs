2376023f9be90f03794ec32ce6b83b8d2cf993c3
Jeremy Banks <_@jeremy.ca>
2021-11-03 17:08:16 +0000

    r25 eight delta foxtrot nine

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(generic_const_exprs)]
+
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value as Json};
 
@@ -13,24 +15,67 @@ pub async fn main() {
 
     let mut spider = crate::spider::Spider::new(google_cookie, ());
 
-    spider.crawl().await
+    let record = json!("a value");
+
+    let key = append_id(
+        api_cache_key("foo", record),
+        api_cache.generate_id().unwrap(),
+    );
+
+    println!("cache_key: {}", printable(&key, '_'));
 }
 
-fn api_cache_key(method_id: &str, parameters: Vec<Json>) -> [u8; 256] {
-    let mut key = [b'_'; 256];
-    let rest = &mut key;
+fn printable(bytes: &[u8], filler: char) -> String {
+    regex::Regex::new(r"[^\ -\~]")
+        .unwrap()
+        .replace_all(
+            &String::from_utf8_lossy(&bytes).to_string(),
+            filler.to_string(),
+        )
+        .to_string()
+}
 
-    let (key_prefix, rest) = rest.split_at_mut(16);
-    let prefix = "api_cache";
-    key_prefix[..prefix.len()].copy_from_slice(prefix.as_bytes());
+fn append_id<const T: usize>(array: [u8; T], id: u64) -> [u8; T + 8] {
+    let mut result = [0u8; T + 8];
+    result[..T].copy_from_slice(&array);
+    result[T..].copy_from_slice(&id.to_be_bytes());
+    result
+}
 
-    let (key_method_id, rest) = rest.split_at_mut(16);
-    key_method_id[..method_id.len()].copy_from_slice(method_id.as_bytes());
+fn api_cache_key(method_id: &str, parameters: Json) -> [u8; 128] {
+    let mut key = [0u8; 128];
 
-    let (key_parameters, rest) = rest.split_at_mut(224);
-    let parameters_json = json!(parameters).to_string();
-    key_parameters[..parameters_json.len()].copy_from_slice(parameters_json.as_bytes());
+    let key_prefix = "api_cache_".as_bytes();
+    key[0..10].copy_from_slice(key_prefix);
 
-    assert!(rest.is_empty());
+    let key_method_id: [u8; 10] = fit_into_array(method_id.as_bytes());
+    key[10..20].copy_from_slice(&key_method_id);
+
+    let parameters_json = parameters.to_string();
+    let key_parameters: [u8; 108] = fit_into_array(parameters_json.as_bytes());
+    key[20..128].copy_from_slice(&key_parameters);
+
     key
+}
+
+fn fit_into_array<const T: usize>(value: &[u8]) -> [u8; T] {
+    let mut array = [0x00; T];
+
+    if value.len() <= T {
+        // If the value fits in the array, great, put it in.
+        // If it's shorter than the array, we'll leave trailing zero-bytes.
+        array[..value.len()].copy_from_slice(value);
+    } else {
+        // If the value's too large to fit in the array, hash it with blake3.
+        let mut hasher = blake3::Hasher::new();
+        hasher.update(value);
+        let mut result = hasher.finalize_xof();
+
+        // Fill the first half of the array with value, truncated to fit.
+        array.copy_from_slice(&value[..T / 2]);
+        // Fill the the second half from the hash digest.
+        result.fill(&mut array[T / 2..]);
+    }
+
+    array
 }
