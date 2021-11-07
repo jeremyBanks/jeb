404f02f86bfe15c16b56bb7f133f9a45bb8ad1dc
Jeremy Banks <_@jeremy.ca>
2021-11-07 12:05:20 +0000

    r38 five four foxtrot alfa

--- src/client.rs
+++ src/client.rs
@@ -285,12 +285,12 @@ impl Client {
         Ok(response)
     }
 
-    pub async fn store_sku(&mut self, sku_id: &str) -> eyre::Result<protos::StoreSkuResponse> {
+    pub async fn store_sku(&mut self, sku_id: &str) -> eyre::Result<Option<protos::StoreSkuResponse>> {
         let response = self.api_request("FWhQV", &json!([null, sku_id])).await;
         let response = response.unwrap();
         tracing::debug!("{}", &response.to_string()[..100]);
 
-        Ok(serde_json::from_str::<protos::StoreSkuResponse>(
+        Ok(serde_json::from_str::<Option<protos::StoreSkuResponse>>(
             &response.to_string(),
         )?)
     }
