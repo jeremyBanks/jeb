1c418176ae57a0f6deefb9dedde7e175e3ea523c
Jeremy Banks <_@jeremy.ca>
2025-11-24 04:16:38 +0000

    wip

--- crates/jeb/src/bin/jeb.rs
+++ crates/jeb/src/bin/jeb.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use {
+    bytes::Bytes,
     color_eyre::Report,
     derive_more::From,
     std::{
@@ -11,88 +12,158 @@ use {
 };
 
 #[derive(Debug, Clone, Default)]
-struct JsonRecord(indexmap::IndexMap<String, JsonValue>);
+struct JsonObject(indexmap::IndexMap<String, JsonValue>);
 
 #[derive(Debug, Clone, Default)]
 enum JsonValue {
     #[default]
     Null,
     Bool(bool),
-    Integer(i64),
+    Unsigned(u64),
+    Signed(i64),
     Float(f64),
     String(String),
     Array(Vec<JsonValue>),
-    Object(JsonRecord),
+    Object(JsonObject),
 }
 
-#[derive(Debug, Clone)]
-enum Item {
-    Bytes(Vec<u8>),
-    Record(JsonRecord),
+fn self_(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    let own_path = std::env::current_exe()?;
+    let own_data = std::fs::read(own_path)?;
+    state.push(Bytes::from(own_data));
+    Ok(state)
 }
 
-pub fn main() {
+fn stdin(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    let mut buffer = Vec::<u8>::new();
+    std::io::stdin().read_to_end(&mut buffer)?;
+    state.push(Bytes::from(buffer));
+    Ok(state)
+}
+
+fn stdout(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    for item in state {
+        std::io::stdout().write_all(&item)?;
+    }
+    Ok(Vec::new())
+}
+
+fn encode_z85(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    state.iter_mut().for_each(|b| *b = jeb::encode_z85(b).into());
+    Ok(state)
+}
+
+fn first(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    while (state.len() > 1) {
+        state.pop();
+    }
+    Ok(state)
+}
+
+fn last(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    let last = state.pop();;
+    state.clear();
+    if let Some(item) = last {
+        state.push(item);
+    }
+    Ok(state)
+}
+
+fn split_lines(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    let mut result = Vec::<Bytes>::new();
+    for bytes in state {
+        for line in bytes.split(|&byte| byte == b'\n') {
+            result.push(Bytes::from(line.to_vec()));
+        }
+    }
+    Ok(result)
+}
+
+fn split_64(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    let mut result = Vec::<Bytes>::new();
+    for bytes in state {
+        let chunks = bytes.chunks(64);
+        for chunk in chunks {
+            result.push(Bytes::from(chunk.to_vec()));
+        }
+    }
+    Ok(result)
+}
+
+fn split_80(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    let mut result = Vec::<Bytes>::new();
+    for bytes in state {
+        let chunks = bytes.chunks(80);
+        for chunk in chunks {
+            result.push(Bytes::from(chunk.to_vec()));
+        }
+    }
+    Ok(result)
+}
+
+fn split_64k(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    let mut result = Vec::<Bytes>::new();
+    for bytes in state {
+        let chunks = bytes.chunks(65536);
+        for chunk in chunks {
+            result.push(Bytes::from(chunk.to_vec()));
+        }
+    }
+    Ok(result)
+}
+
+fn join(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    let input = take(&mut state).into_iter().flatten().collect::<Vec<u8>>();
+    state.push(Bytes::from(input));
+    Ok(state)
+}
+
+fn join_lines(mut state: Vec<Bytes>) -> Result<Vec<Bytes>, Report> {
+    let input = take(&mut state)
+        .into_iter()
+        .flat_map(|b| b.into_iter().chain(std::iter::once(b'\n')))
+        .collect::<Vec<u8>>();
+    state.push(Bytes::from(input));
+    Ok(state)
+}
+
+#[tokio::main(flavor = "current_thread")]
+pub async fn main() -> Result<(), Report> {
     let mut args = Vec::<String>::from_iter(std::env::args());
     let own_path: String = args.remove(0);
-
-    let commands =
-        HashMap::<&'static str, fn(&mut Vec<Item>) -> Result<(), Report>>::from_iter(vec![
-            ("stdin", |state: &mut Vec<Item>| -> Result<(), Report> {
-                let mut buffer = Vec::<u8>::new();
-                std::io::stdin().read_to_end(&mut buffer)?;
-                state.push(Item::Bytes(buffer));
-                Ok(())
-            }),
-            ("stdout", |state: &mut Vec<Item>| -> Result<(), Report> {
-                for item in take(state) {
-                    match item {
-                        Item::Bytes(blob) => {
-                            std::io::stdout().write_all(&blob)?;
-                        }
-                        Item::Record(_) => {
-                            return Err(Error::msg("cannot write non-bytes item to stdout"));
-                        }
-                    }
-                }
-                Ok(())
-            }),
-        ]);
 
     if (args.is_empty() || args.iter().any(|arg| arg == "--help" || arg == "-h")) {
         eprint!("usage: {own_path} [--help|-h]");
         eprintln!();
-        return;
+        return Ok(());
     }
 
-    let mut state = Vec::<Vec<u8>>::new();
+    let mut state = Vec::<Bytes>::new();
+
+    eprintln!("{own_path} {}", args.join(" "));
 
     for arg in args {
-        match arg.as_str() {
-            "stdin" => {
-                let mut buffer = Vec::<u8>::new();
-                std::io::stdin().read_to_end(&mut buffer).unwrap();
-                state.push(buffer);
-            }
-
-            "self" => {
-                let own_data = std::fs::read(&own_path).unwrap();
-                state.push(own_data)
-            }
-
-            "encode-z85" => {
-                let input = take(&mut state).into_iter().flatten().collect::<Vec<u8>>();
-                let encoded = jeb::encode_z85(&input);
-                state.push(encoded);
-            }
-
-            _ => {
+        state = match arg.as_str() {
+            "stdin" => stdin(state)?,
+            "stdout" => stdout(state)?,
+            "self" => self_(state)?,
+            "first" => first(state)?,
+            "last" => last(state)?,
+            "split-lines" => split_lines(state)?,
+            "split-64" => split_64(state)?,
+            "split-80" => split_80(state)?,
+            "split-64k" => split_64k(state)?,
+            "join" => join(state)?,
+            "join-lines" => join_lines(state)?,
+            "encode-z85" => encode_z85(state)?,
+            arg => {
                 eprintln!("error: unrecognized argument: {arg}");
                 std::process::exit(1);
             }
         }
     }
 
-    for blob in state {
-        std::io::stdout().write_all(&blob).unwrap();
-    }
+    println!();
+
+    Ok(())
 }
