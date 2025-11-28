a24f09c6662f23bce957a46c249dcc5fe44d96fc
jeremyBanks <18020+jeremyBanks@users.noreply.github.com>
2025-11-28 23:27:14 +0000

    Apply automatic changes

--- crates/jeb/examples/help.sh
+++ crates/jeb/examples/help.sh
@@ -13,12 +13,12 @@ JEB
 
 
 |# `jwNY3O3qak)1|N Entity Bag?kmd-&2|ust Encode Bytes?|kmd-&3|oined Escaped Bina
-ry?|.kmd-83jp@<d|be-coded clanker slop with Semitranslucent Binary Encodings.|..
-..........3jmaE|## LicensB7CW=8|Copyright Jeremy Banks and contributors.|.......
-3joEv3|censed under either of:z!q3q3n0w^g|pache License, Version 2.0 (<http://ww
-w.apache.org/licenses/LICENSE-2.0>)|..............dgdTHa|MIT license (<http://op
-ensource.org/licenses/MIT>)|.......j$c*C1|at your optioz/dfm3l{=r1|Contribution|
-3jo^Jh|less you explicitly state otherwise, any contribution intentionally submi
-tted|...............wc#{!h|r inclusion in the work by you, as defined in the Apa
-che-2.0 license, shall be|..............vR2)Se|ual licensed as above, without an
-y additional terms or conditions.|...........B1N!
+ry?|.kmd:vd|ibe-coded clanker slop with Semitranslucent Binary Encodings.|......
+.....e^.}g|# LicensewEn=i7|opyright Jeremy Banks and contributors.|...A=S&u3qtez
+3|ensed under either of:|w&Znah|- Apache License, Version 2.0 (<http://www.apach
+e.org/licenses/LICENSE-2.0>)|................3n0w{a|IT license (<http://opensour
+ce.org/licenses/MIT>)|........dgcz<1|t your optionzy#n61|## Contributix(v>@3rp$W
+h|ess you explicitly state otherwise, any contribution intentionally submitted|.
+...............3tai%h| inclusion in the work by you, as defined in the Apache-2.
+0 license, shall be|...............wEq%<e|al licensed as above, without any addi
+tional terms or conditions.|............e^.
