a33adc7cc94b15dc7a476cdad4c0eb1dc101a834
Jeremy Banks <_@jeremy.ca>
2025-12-30 18:38:18 +0000

    xC730

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,8 +9,8 @@ git config --global pull.rebase false
 git config --global push.autoSetupRemote true
 git config --global core.pager "less -F -X"
 # shellcheck disable=SC2016
-git config --global alias.save "\!$(cat scripts/git-save.sh)"
-git config --global alias.message "\!$(cat scripts/git-message.sh)"
+git config --global alias.save "\!$PWD/scripts/git-save.sh"
+git config --global alias.message "\!$PWD/scripts/git-message.sh"
 
 rustup update
 rustup show
