d79e23f8dadf3432877fc5d0012a9ee8b83adbc8
Jeremy Banks <_@jeremy.ca>
2026-01-01 23:20:34 +0000

    x75D8

--- crates/_autofix/src/lib.rs
+++ crates/_autofix/src/lib.rs
@@ -5,5 +5,4 @@ pub mod cargo_fmt;
 pub mod command_runner;
 pub mod deno_fmt;
 pub mod deno_lint;
-pub mod prettyplease;
 pub mod workspace_deps;
--- crates/_autofix/src/main.rs
+++ crates/_autofix/src/main.rs
@@ -1,10 +1,8 @@
 use _autofix::{
-    autofix_runner, cargo_clippy, cargo_fix, cargo_fmt, deno_fmt, deno_lint,
-    prettyplease, workspace_deps,
+    autofix_runner, cargo_clippy, cargo_fix, cargo_fmt, deno_fmt, deno_lint, workspace_deps,
 };
 fn main() {
     let modules: &[(&str, fn() -> i32)] = &[
-        ("prettyplease", prettyplease::main),
         ("cargo_fmt", cargo_fmt::main),
         ("cargo_fix", cargo_fix::main),
         ("cargo_clippy", cargo_clippy::main),
