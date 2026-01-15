53e62b29636bffd76943459da02b0ad2dd06dc72
Jeremy Banks <_@jeremy.ca>
2026-01-15 21:28:13 +0000

    x49F5

--- crates/jeb/src/spec.rs
+++ crates/jeb/src/spec.rs
@@ -7,31 +7,31 @@ use inline::*;
 
 #[test]
 fn literate() {
-    // # Encoding bytes as text
+    /// # Encoding bytes as text
 
-    // There are a lot of different ways to encode binary data as text, with
-    // different trade-offs and use cases. The most common considerations as
-    // size/efficiency (how many characters are required to encode a given
-    // number of bytes), and what characters are used in the encoded
-    // representation (determining contexts where the encoded data can be used).
+    /// There are a lot of different ways to encode binary data as text, with
+    /// different trade-offs and use cases. The most common considerations as
+    /// size/efficiency (how many characters are required to encode a given
+    /// number of bytes), and what characters are used in the encoded
+    /// representation (determining contexts where the encoded data can be
+    /// used).
 
-    // ## Latin-1 passthrough
+    /// ## Latin-1 passthrough
 
-    // The simplest possible way to encode binary data as text is just to covert
-    // byte values directly to Unicode code points. This is sometimes referred
-    // to **Latin-1 passthrough**, because this is equivalent to the legacy
-    // "Latin-1" text encoding whose characters now make up the first 256
-    // Unicode code points (the "Basic Latin" (ASCII) and
-    // "Latin-1 Supplement" blocks).
+    /// The simplest possible way to encode binary data as text is just to
+    /// covert byte values directly to Unicode code points. This is
+    /// sometimes referred to **Latin-1 passthrough**, because this is
+    /// equivalent to the legacy "Latin-1" text encoding whose characters
+    /// now make up the first 256 Unicode code points (the "Basic Latin"
+    /// (ASCII) and "Latin-1 Supplement" blocks).
 
-    // (We'll be focusing primarily on byte-oriented encodings that can produce
-    // ASCII-safe output.)
+    /// (We'll be focusing primarily on byte-oriented encodings that can produce
+    /// ASCII-safe output.)
 
-    // One of the simplest as most common ways is hexadecimal ("hex", base 16).
-    // Each byte is eight bits, which evenly divides into two hex digits.
+    /// One of the simplest as most common ways is hexadecimal ("hex", base 16).
+    /// Each byte is eight bits, which evenly divides into two hex digits.
 
-    // bits, and each hex digit represents four bits, so we just output two
-    // hex digits for each byte.
-
+    /// bits, and each hex digit represents four bits, so we just output two
+    /// hex digits for each byte.
     let _ = ();
 }
