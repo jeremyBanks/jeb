84d7f3aec5c3fada4190e99b7b3fd2f3d6058db1
Jeremy Banks <_@jeremy.ca>
2026-01-20 07:01:09 +0000

    xBB5C

--- crates/_examples/examples/example.rs
+++ crates/_examples/examples/example.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use std::fmt::Debug;
+
+macro_rules! alias {
+    ($vis:vis impl $name:ident: $($rest:tt)+) => {
+        #[doc = concat!("Alias for ", stringify!($($rest)+))]
+        $vis trait $name: $($rest)+ {}
+        impl<T: $($rest)+> $name for T {}
+    };
+    ($vis:vis dyn $name:ident: $($rest:tt)+) => {
+        alias! { $vis impl $name: $($rest)+ }
+        const _: Option<Box<dyn $name>> = None;
+    };
+}
+
+alias! { pub impl EqPartialEq: Debug + CloneDebug }
+alias! { pub dyn CloneDebug: Debug }
+
+fn main() {
+    // CloneDebug
+}
