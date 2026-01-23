2159c0e209016b78308a5807b71c2017d6a823ff
Jeremy Banks <_@jeremy.ca>
2026-01-23 21:42:53 +0000

    r179 / g1405 / n2159 / x529A / oCD2E

--- crates/jeb-tracing/src/lib.rs
+++ crates/jeb-tracing/src/lib.rs
@@ -13,6 +13,17 @@ pub static ENABLED: OnceLock<bool> = OnceLock::new();
 pub static LOG_ENV: LazyLock<String> = LazyLock::new(|| {
     let common = std::env::var("RUST_LOG");
     let specific = std::env::var("JEB_LOG");
+
+    let default_log_level = "warn";
+
+    let verbose_crates = [
+        option_env!("CARGO_CRATE_NAME"),
+        option_env!("CARGO_PKG_NAME"),
+        option_env!("CARGO_BIN_NAME"),
+    ]
+    .flatten()
+    .collect::<Vec<&str>>();
+
     let default = "warn,jeb=info,save=info".to_string();
 
     specific.or(common).unwrap_or(default)
