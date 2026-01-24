4335f3ff772395d31088b789c50a41f04d3f5691
Jeremy Banks <_@jeremy.ca>
2026-01-24 00:00:34 +0000

    r191 / g1423 / n4335 / x0AF6 / oCD2E

--- crates/inline/src/lib.rs
+++ crates/inline/src/lib.rs
@@ -1,5 +1,3 @@
-#![cfg_attr(not(test), forbid(unsafe_code))]
-
 //! Self-modifying values that update their source code at runtime.
 //!
 //! `inline` provides smart pointers that can modify their own source code.
