7291d1e31c566b52daf5a2dcb76e6d2ff1a16ab3
Jeremy Banks <_@jeremy.ca>
2026-01-26 01:16:20 +0000

    r266 / g1638 / n7291 / x8815 / oCD2E

--- crates/_examples/fuzz/fuzz_targets/fuzz_json.rs
+++ crates/_examples/fuzz/fuzz_targets/fuzz_json.rs
@@ -3,7 +3,7 @@
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let data = &data[..data.len().min(32)];
+    let data = &data[..data.len().min(16)];
 
     // Try to decode as UTF-8
     let Ok(text) = std::str::from_utf8(data) else {
