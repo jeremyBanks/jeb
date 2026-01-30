528a65c6e084bf5dfaad585a43df52b5ff9d00b9
Jeremy Banks <_@jeremy.ca>
2026-01-30 08:11:00 +0000

    Fix brainstorm example to use v2 API

--- crates/zipng/examples/brainstorm.rs
+++ crates/zipng/examples/brainstorm.rs
@@ -1,30 +1,23 @@
-// XXX: AGENTS, IGNORE THIS FILE! I am a human working on it
-// while you're doing other things. If this file causes something to
-// break for you, please immediately stop and let me know, do not attempt
-// to edit this file yourself, or attempt overcomplicated workarounds.
-// It's okay if this file is included when you commit, and it's okay if
-// not. Don't worry about it either way.
+// Sketch of the v2 public API usage.
 
+use std::collections::HashMap;
 
-use alloc::collections::HashMap;
-
-#[cfg(feature = "ignore-me")]
 fn main() {
-    let files = HashMap::<&'static [u8], Vec<&Vec<u8>>>::new();
-    
-    // `::encode` and `.encode` takes a very generic input type of basically anything that can
-    // be iterate over as pairs of values that can each be borrowed as `&[u8]`, which may be
+    let files = HashMap::<&'static [u8], Vec<u8>>::new();
+
+    // `::encode` and `.encode` take a very generic input type of basically anything that can
+    // be iterated over as pairs of values that can each be borrowed as `&[u8]`, which may be
     // different types.
 
-    let encoded_with_defaults: Vec<u8> = zipng::encode(files);
-    let encoded_with_config: Vec<u8> = zipng::Encoder::new()
-        .with_mode(zipng::EncoderMode::Rgba)
-        .with_font(zipng::Font::SixthSlab)
-        .encode(files);
+    let _encoded_with_defaults: Vec<u8> = zipng::v2::encode(&files);
+    let _encoded_with_config: Vec<u8> = zipng::v2::Encoder::new()
+        .with_mode(zipng::v2::EncoderMode::Rgba)
+        .with_font(zipng::v2::FontChoice::Sixth)
+        .encode(&files);
 
-    // Decode returns impl FromIterator<(Vec<u8>, Vec<u8>)> or something like that (still generic, but less generic).
-    // Generic defaults are allowed in this case we'd default to be BTreeMap but I don't think they are.
-    let decoded: BTreeMap<Vec<u8>, Vec<u8>> = zipng::decode(encoded_with_defaults);
-    let decoded_with_config: Vec<u8> = zipng::Decoder::new()
-        .with_source(zipng::Source::Zip);
+    // Decode is not yet implemented but the API shape exists.
+    // let decoded: Vec<(Vec<u8>, Vec<u8>)> = zipng::v2::decode(&encoded_with_defaults);
+    // let decoded_with_config = zipng::v2::Decoder::new()
+    //     .with_source(zipng::v2::Source::Zip)
+    //     .decode(&encoded_with_config);
 }
