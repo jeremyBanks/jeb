1773ef2018afbcd7cba8645967c503d9578ab1b7
mattemoon <mattemoon@fastmail.com>
2026-02-13 03:14:55 +0000

    remove haiku-6-ref: failed to implement mid-block cuts, just block-aligned with 3-char overhead

--- crates/jeb/examples/haiku-6-ref/src/main.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    // This is a library-only project; use cargo test to run tests
-    println!("Use: cargo test");
-}
---
+++ crates/jeb/examples/haiku-6-ref/target/debug/deps/z85-8e84fe3deffe2439.d
@@ -0,0 +1,5 @@
+/Users/matte/jeb/crates/jeb/examples/haiku-6-ref/target/debug/deps/z85-8e84fe3deffe2439.d: src/main.rs
+
+/Users/matte/jeb/crates/jeb/examples/haiku-6-ref/target/debug/deps/z85-8e84fe3deffe2439: src/main.rs
+
+src/main.rs:
