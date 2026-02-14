93c574cc42bb73c7a36ea9ef100a1bda9a8678cb
Jeremy Banks <_@jeremy.ca>
2026-02-14 04:18:41 +0000

    r93 / x8C2A eight charlie two alfa / oAE5B

---
+++ min.mjs
@@ -0,0 +1,7 @@
+export default {encode,decode}
+/**@returns {string}*/export function encode(/**@type {Uint8Array}*/_original) {
+throw new Error('Not implemented');
+}
+/**@returns {Uint8Array}*/export function decode(/**@type {string}*/_encoded) {
+throw new Error('Not implemented');
+}
